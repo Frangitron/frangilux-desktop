b74f8f5510ebea58c2753c7dbffef617a55c0fe9
MrFrangipane <contact@frangipane.me>
2025-06-15 21:45:30 +0200

    wip(ClipEditor): references

--- frangiluxdesktop/widgets/clip_editor/point_editor.py
+++ frangiluxdesktop/widgets/clip_editor/point_editor.py
@@ -1,8 +1,10 @@
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QGroupBox, QCheckBox, QVBoxLayout, QLineEdit, QComboBox
+from PySide6.QtWidgets import QGroupBox, QCheckBox, QVBoxLayout, QLineEdit, QComboBox, QPushButton
 
 from frangiluxlib.components.clip_point import ClipPoint
 from frangiluxlib.components.clip_point_reference_store import ClipPointReferenceStore
+
+from pyside6helpers import combo
 
 
 class PointEditor(QGroupBox):
@@ -27,10 +29,16 @@ class PointEditor(QGroupBox):
         self.combobox_reference.addItems(self._reference_store.references_names())
         self.combobox_reference.currentIndexChanged.connect(self._update_point)
 
+        self.lineedit_new_reference_name = QLineEdit()
+        self.button_new_reference = QPushButton("Make reference")
+        self.button_new_reference.clicked.connect(self._new_reference)
+
         layout = QVBoxLayout(self)
         layout.addWidget(self.checkbox_reference)
         layout.addWidget(self.combobox_reference)
         layout.addWidget(self.checkbox_reference_edit)
+        layout.addWidget(self.lineedit_new_reference_name)
+        layout.addWidget(self.button_new_reference)
 
         self.setEnabled(False)
         self.setFixedWidth(200)
@@ -62,3 +70,18 @@ class PointEditor(QGroupBox):
         self._point.is_reference_editable = self.checkbox_reference_edit.isChecked()
 
         self.ValueChanged.emit()
+
+    def _new_reference(self):
+        if self._point is None:
+            return
+
+        name = self.lineedit_new_reference_name.text()
+        if name == "":
+            return
+
+        self._reference_store.new(name, self._point)
+        self._point.reference_name = name
+        self._point.is_reference = True
+
+        combo.update(self.combobox_reference, self._reference_store.references_names())
+        self.set_point(self._point)  # FIXME meh
